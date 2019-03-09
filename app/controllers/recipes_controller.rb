67d0bdc00509eb9b7927d5331dd38db041a210fd
Eduhaydee <webmaster.kdu@gmail.com>
2019-03-09 20:30:11 -0300

    User register recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,5 +3,21 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def new
+    @recipe = Recipe.new
+  end
   
+  def create
+    @recipe = Recipe.create(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    end
+  end
+  
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, :recipe_type, :cuisine, :difficulty, :cook_time, :ingredients, :cook_method)
+  end
+
 end
