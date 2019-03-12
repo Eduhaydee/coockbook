fcbc9f3fa9c5528a21ee0e904cb65c1914e2c19d
Eduhaydee <webmaster.kdu@gmail.com>
2019-03-12 10:03:03 -0300

    View last recipes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < RecipesController
     @recipes = Recipe.all
     @recipe_featured = Recipe.where(featured: true)
     @recipe_unfeatured = Recipe.where(featured: false)
+    @last_recipes = Recipe.where.not(id: @recipe_featured.map(&:id).concat(@recipe_unfeatured.map(&:id))).order(created_at: :desc).limit(10)
     #@recipe_types = RecipeType.all
   end
 
